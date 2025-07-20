344bdfcc5b1bc864d95777249d6955bcf0a1414a
Kieran Hannigan <overyonderstudios@gmail.com>
2025-07-20 15:10:20 +1000

    fixup

--- rust/pong_ai/src/main.rs
+++ rust/pong_ai/src/main.rs
@@ -1,16 +1,22 @@
-use macroquad::{
-    miniquad::window::{self, quit},
-    prelude::*,
-};
+#[cfg(not(target_arch = "wasm32"))]
+use macroquad::miniquad::window;
+use macroquad::{miniquad::window::quit, prelude::*};
 use pong_ai::{
     constants::*,
     drawing::*,
     game::{
         controllers::Player,
-        state::{FRAME_RATE, Game, Side},
+        state::{Game, Side},
     },
     nn::{Group, Individual},
 };
+
+#[derive(PartialEq)]
+enum ControlMode {
+    AA, // AI vs AI
+    AH, // AI vs Human
+    HH, // Human vs Human
+}
 
 #[macroquad::main("MyGame")]
 async fn main() {
@@ -19,11 +25,11 @@ async fn main() {
     window::set_fullscreen(true);
     next_frame().await;
     let mut debugging: bool = true;
-    let _left = &Player {
+    let left_player = &Player {
         up_key: KeyCode::W,
         down_key: KeyCode::S,
     };
-    let _right = &Player {
+    let right_player = &Player {
         up_key: KeyCode::Up,
         down_key: KeyCode::Down,
     };
@@ -39,7 +45,6 @@ async fn main() {
     let mut score;
 
     for generation in 0..GENERATIONS {
-
         if is_key_released(KeyCode::Q) {
             println!(
                 "Best weights: {:?}",
@@ -81,30 +86,62 @@ async fn main() {
         group.individuals()[POP_SIZE - 1].weights()
     );
 
-    let left = &former_champion;
-    let right = &current_champion;
+    let left_ai = &former_champion;
+    let right_ai = &current_champion;
     let mut game = Game::default();
 
+    let mut control_mode = ControlMode::AA;
+
     loop {
-        if is_key_released(KeyCode::Q) {
+        if is_key_pressed(KeyCode::Q) {
             quit();
-        } else if is_key_released(KeyCode::N) {
+        } else if is_key_pressed(KeyCode::N) {
             debugging = !debugging;
+        } else if is_key_pressed(KeyCode::P) {
+            control_mode = match control_mode {
+                ControlMode::AA => ControlMode::AH,
+                ControlMode::AH => ControlMode::HH,
+                ControlMode::HH => ControlMode::AA,
+            };
         }
         if get_time() - delta < 0.015 {
             continue;
         }
 
         clear_background(BLACK);
-        draw_game(
-            &debugging,
-            screen_width(),
-            screen_height(),
-            &game,
-            left,
-            right,
-        );
-        game.tick(left, right);
+
+        if control_mode == ControlMode::AA {
+            draw_game(
+                &debugging,
+                screen_width(),
+                screen_height(),
+                &game,
+                left_ai,
+                right_ai,
+            );
+            game.tick(left_ai, right_ai);
+        } else if control_mode == ControlMode::AH {
+            draw_game(
+                &debugging,
+                screen_width(),
+                screen_height(),
+                &game,
+                left_ai,
+                right_player,
+            );
+            game.tick(left_ai, right_player);
+        } else {
+            draw_game(
+                &debugging,
+                screen_width(),
+                screen_height(),
+                &game,
+                left_player,
+                right_player,
+            );
+            game.tick(left_player, right_player);
+        }
+
         next_frame().await;
         delta = get_time();
     }
